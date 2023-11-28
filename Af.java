865a51a97e500bd82952cd7f779ed955c3e0e235
GUSTAVO POSSIDONIO <152180279+Gustavoposss@users.noreply.github.com>
2023-11-28 08:47:42 -0300

    Update Af.java

--- Af.java
+++ Af.java
@@ -2,7 +2,7 @@ package atividade;
 
 public class Af {
 
-	public void metodoUm() {
+	 public void metodoUm() {
         System.out.println("Este é o método Um.");
     }
 
@@ -11,5 +11,13 @@ public class Af {
 
         // Chamada do método Um dentro do método Dois
         metodoUm();
+    }
+
+    public static void main(String[] args) {
+        // Criar uma instância da classe
+        MinhaClasse minhaInstancia = new MinhaClasse();
+
+        // Chamar o método Dois
+        minhaInstancia.metodoDois();
     }
 }
